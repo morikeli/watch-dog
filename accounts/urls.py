2ade195826aa49364400f1708b85014423ce6f7b
brocode <kelimbth@gmail.com>
2024-02-03 20:12:12 +0300

    url paths

---
+++ accounts/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+from . import validators
+
+
+urlpatterns = [
+    path('login/', views.UserLoginView.as_view(), name='login'),
+    path('signup/', views.SignupView.as_view(), name='signup'),
+    path('profile/', views.UserProfileView.as_view(), name='user_profile'),
+    path('logout/', views.LogoutUserView.as_view(), name='logout_user'),   
+]
+
+htmx_urlpatterns = [
+    path('validate-username/', validators.check_username_exists, name='validate_username'),
+    path('validate-email/', validators.check_email_exists, name='validate_email'),
+    path('validate-age/', validators.date_and_users_age_validation, name='validate_age'),
+    path('check-password/', validators.password_match_and_length_validation, name='check_password'),
+]
+
+urlpatterns += htmx_urlpatterns
