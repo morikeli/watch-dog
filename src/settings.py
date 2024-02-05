3d5a4c55ad139fb7fce4b3196ac46f2418a726b6
brocode <kelimbth@gmail.com>
2024-02-05 14:27:41 +0300

    add MEDIA_URL and MEDIA_ROOT

--- src/settings.py
+++ src/settings.py
@@ -119,6 +119,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR, 'static']
 
+MEDIA_URL = '/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
