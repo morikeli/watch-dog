12da032670f59ae56b5eb0f9f8c8c534276543e0
brocode <kelimbth@gmail.com>
2024-02-05 14:09:47 +0300

    minor changes: 1.) import dependencies 2.) create HomepageView

--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.views import View
 
-# Create your views here.
+
+class HomepageView(View):
+    template_name = ''
+
+    def get(self, request, *args, **kwargs):
+
+        context = {}
+        return render(request, self.template_name, context)
