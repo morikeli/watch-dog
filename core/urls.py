abacb58922f660bb8ef3a211910e197b4e9782fc
brocode <kelimbth@gmail.com>
2024-02-08 17:19:59 +0300

    add url path

--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('homepage/', views.HomepageView.as_view(), name='homepage'),
     path('map/', views.GeoMapView.as_view(), name='geo_map'),
     path('incidents/', views.IncidentsStatisticsView.as_view(), name='reported_incidents'),
-    
+    path('report-incident/', views.ReportIncidentsView.as_view(), name='report_incident'),
 ]
