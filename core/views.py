708f1423081d325c630c68da15cadf0e9394c3b1
brocode <kelimbth@gmail.com>
2024-02-08 16:15:43 +0300

    import and add decorators to views

--- core/views.py
+++ core/views.py
@@ -1,11 +1,14 @@
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.utils.decorators import method_decorator
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.views import View
 from .forms import ReportIncidentForm, SubmitLocationForm, ReportRoadAccidentForm, ReportFireIncidentForm, ReportCrimesForm
+import folium
 
 
+@method_decorator(login_required(login_url='login'), name='get')
+@method_decorator(user_passes_test(lambda user: user.is_staff is False and user.is_superuser is False), name='get')
 class HomepageView(View):
     template_name = 'core/homepage.html'
 
