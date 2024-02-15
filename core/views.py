3f70564d557a9d5f6e3dab2abc3c792a1efc1af1
brocode <kelimbth@gmail.com>
2024-02-15 21:37:24 +0300

    update map config., pinpoint accident locations & add markers

--- core/views.py
+++ core/views.py
@@ -36,11 +36,41 @@ class GeoMapView(View):
             attr='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>',
             min_lat=0.0126,
             max_lat=5.2,
-            control_scale=True, 
             zoom_start=7, 
             min_zoom=7,
             max_zoom=14,
         ).add_to(map_figure)
+        marker_cluster = MarkerCluster(name='Blackspots clusters').add_to(geo_map)
+
+        for spot in accident_spots:     # get each record in the queryset
+            lat_coord = 'S' if spot.location_id.latitude < 0 else 'N'
+            popup_text = f"""
+                <div></div>
+                <div class="table-responsive mt-3">
+                    <table class="table table-sm table-hover table-striped table-condensed table-bordered">
+                        <tbody>
+                            <tr><td class="fw-bold">Coordinates</td><td>{ spot.location_id.latitude }&deg; {lat_coord}, { spot.location_id.longitude }&deg; E</td></tr>
+                            <tr><td class="fw-bold">County</td><td>{ spot.location_id.county }</td></tr>
+                            <tr><td class="fw-bold">Sub county</td><td>{ spot.location_id.sub_county }</td></tr>
+                            <tr><td class="fw-bold">City</td><td>{ spot.location_id.city }</td></tr>
+                            <tr><td class="fw-bold">Road/Highway</td><td>{ spot.road }</td></tr>
+                            <tr><td class="fw-bold">Road user/Victim</td><td>{ spot.road_user }</td></tr>
+                            <tr><td class="fw-bold">Total injuries</td><td>{ spot.injuries_count }</td></tr>
+                        </tbody>
+                    </table>
+                </div>
+            """
+
+            # markers
+            folium.Marker(
+                location=[spot.location_id.latitude, spot.location_id.longitude],
+                tooltip='Click marker for more info.',
+                popup=popup_text,
+                icon=folium.Icon(icon='exclamation-triangle', color='red', prefix='fa', icon_color='#ffa500'),
+            ).add_to(marker_cluster)
+        
+        folium.LayerControl().add_to(geo_map)
+
         
         context = {
             'Map': geo_map._repr_html_,
