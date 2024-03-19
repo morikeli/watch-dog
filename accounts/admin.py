a5b26fa37142589be7102bfafb0904beff1c58c1
brocode <kelimbth@gmail.com>
2024-03-19 13:58:24 +0300

    imported new model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,7 @@
 from .models import User
 from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
-from .models import User
+from .models import User, Officer
 from .forms import SignupForm
 
 
