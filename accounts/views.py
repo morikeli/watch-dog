d7105ab2adde0e54f8133f7c4f216a49990c1f8c
brocode <kelimbth@gmail.com>
2024-03-19 14:16:44 +0300

    imported new dependencies

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
 from django.utils.decorators import method_decorator
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
