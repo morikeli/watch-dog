91a6ec91b346fa68de17ee95659f8fe0f65d5153
brocode <kelimbth@gmail.com>
2024-03-19 14:35:51 +0300

    deleted replace() and used uuid4().hex

--- core/signals.py
+++ core/signals.py
@@ -7,28 +7,28 @@ import uuid
 @receiver(pre_save, sender=Incident)
 def generate_incidentsID(sender, instance, **kwargs):
     if instance.id == '':
-        instance.id = str(uuid.uuid4()).replace('-', '')[:30]
+        instance.id = str(uuid.uuid4().hex)[:30]
 
 
 @receiver(pre_save, sender=Location)
 def generate_locationID(sender, instance, **kwargs):
     if instance.id == '':
-        instance.id = str(uuid.uuid4()).replace('-', '')[:30]
+        instance.id = str(uuid.uuid4().hex)[:30]
 
 
 @receiver(pre_save, sender=RoadAccident)
 def generate_road_accidentsID(sender, instance, **kwargs):
     if instance.id == '':
-        instance.id = str(uuid.uuid4()).replace('-', '')[:30]
+        instance.id = str(uuid.uuid4().hex)[:30]
 
 
 @receiver(pre_save, sender=FireIncident)
 def generate_fire_incidentID(sender, instance, **kwargs):
     if instance.id == '':
-        instance.id = str(uuid.uuid4()).replace('-', '')[:30]
+        instance.id = str(uuid.uuid4().hex)[:30]
 
 
 @receiver(pre_save, sender=ReportedCrime)
 def generate_reported_crimesID(sender, instance, **kwargs):
     if instance.id == '':
-        instance.id = str(uuid.uuid4()).replace('-', '')[:30]
+        instance.id = str(uuid.uuid4().hex)[:30]
