2b85956fbd1d95036eb9308566c32e775b6f3e4e
brocode <kelimbth@gmail.com>
2024-03-20 11:02:29 +0300

    Revert "--no-edit"
    This reverts commit ed98e615beaf15a3a8ff48fb3eb71c94b69f3c2e.

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('homepage/', views.HomepageView.as_view(), name='homepage'),
     path('map/', views.GeoMapView.as_view(), name='geo_map'),
-    path('incidents/', views.IncidentsDetailView.as_view(), name='reported_incidents'),
+    path('incidents/', views.IncidentsStatisticsView.as_view(), name='reported_incidents'),
     path('report-incident/', views.ReportIncidentsView.as_view(), name='report_incident'),
     
     path('<str:officer_id>/wanted-suspects/', views.WantedSuspectsCreateView.as_view(), name='wanted_suspects'),
