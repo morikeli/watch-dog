ceaf4f9497e0806a98dc91668d050a2e32cf1e99
brocode <kelimbth@gmail.com>
2024-03-22 11:27:30 +0300

    imported new model

--- core/signals.py
+++ core/signals.py
@@ -1,4 +1,4 @@
-from .models import Incident, Location, RoadAccident, FireIncident, ReportedCrime, PoliceStation, WantedSuspect
+from .models import Incident, Location, RoadAccident, FireIncident, ReportedCrime, PoliceStation, WantedSuspect, ReportSuspect
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 import uuid
