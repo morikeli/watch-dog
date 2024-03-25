c1f5665c9d80fb0c7c9dcc5bf75dbb0b069014bc
brocode <kelimbth@gmail.com>
2024-03-25 17:24:04 +0300

    updated view name in url route in urlpatterns

--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('homepage/', views.HomepageView.as_view(), name='homepage'),
     path('map/', views.GeoMapView.as_view(), name='geo_map'),
     path('incidents/', views.IncidentsDetailView.as_view(), name='reported_incidents'),
-    path('report-incident/', views.ReportIncidentsView.as_view(), name='report_incident'),
+    path('report-incident/', views.ReportIncidentsCreateView.as_view(), name='report_incident'),
     path('suspect/<str:suspect_id>/report/', views.ReportWantedSuspectsCreateView.as_view(), name='report_suspect'),
     
     path('wanted-suspects/', views.WantedSuspectsCreateView.as_view(), name='wanted_suspects'),
