41fc22b9e3ce2f4734e917d3be1197118815e57c
brocode <kelimbth@gmail.com>
2024-03-29 17:55:20 +0300

    imported pagination class

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
