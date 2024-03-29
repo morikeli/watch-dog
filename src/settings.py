8724cff3534113035660f1fdfef1a466994d3667
brocode <kelimbth@gmail.com>
2024-03-29 15:42:45 +0300

    updated project configs

--- src/settings.py
+++ src/settings.py
@@ -16,9 +16,9 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [env('WEB_DOMAIN')]
 
 
 # Application definition
