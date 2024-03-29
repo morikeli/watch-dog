21732b2a23d68ce1afac9cbdca3cbd9b9c516c86
brocode <kelimbth@gmail.com>
2024-03-29 14:14:04 +0300

    created new serializers

---
+++ api/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from core.models import Incident
+from accounts.models import User
+
+
+class UserSignupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+
+        if password is not None:
+            instance.set_password(password)
+            instance.save()    
+        return instance
+
+
+class IncidentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Incident
+        fields = '__all__'
