c816fbeb473a242ac03e4681c4bc9d6dbabea03e
brocode <kelimbth@gmail.com>
2024-03-29 14:14:23 +0300

    created views for API

--- api/views.py
+++ api/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from django.contrib import auth
+from .serializers import IncidentSerializer, UserSignupSerializer
+from core.models import Incident
 
-# Create your views here.
+
+class LoginView(APIView):
+    def post(self, request, *args, **kwargs):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = auth.authenticate(email=email, password=password)
+        
+        if user is None:
+            raise AuthenticationFailed('INVALID CREDENTIALS!! Please try again later.')
+        
+
+        return Response({"message": "Welcome back"}, status=status.HTTP_200_OK)
+
+
+class SignupView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = UserSignupSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class IncidentsAPIListView(APIView):
+    def get(self, request, *args, **kwargs):
+        incidents = Incident.objects.all()
+        serializer = IncidentSerializer(incidents, many=True)
+        return Response(serializer.data)
