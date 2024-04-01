ca7d0f63a7b9da1077a9f60ae5bf7ba8aed05b0a
brocode <kelimbth@gmail.com>
2024-04-01 11:13:14 +0300

    updated dependecies

--- src/urls.py
+++ src/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
+from django.views.static import serve
 from django.contrib import admin
 from django.conf import settings
 
