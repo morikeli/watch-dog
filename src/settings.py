b33fbc995674c49870cd6ebc1201b8c5fe8e70db
brocode <kelimbth@gmail.com>
2024-04-03 17:03:58 +0300

    appended new authentication classes

--- src/settings.py
+++ src/settings.py
@@ -157,6 +157,8 @@ CORS_ALLOWED_ORIGINS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
