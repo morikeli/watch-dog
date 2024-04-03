e5b01dcd96bcc0caff9ec60f351b9e9a2a14147f
brocode <kelimbth@gmail.com>
2024-04-03 17:04:45 +0300

    added simple_jwt config.

--- src/settings.py
+++ src/settings.py
@@ -163,3 +163,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
+}
