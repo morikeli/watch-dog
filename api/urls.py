5be59ca3753a8dc6a02db89a2627649000f33686
brocode <kelimbth@gmail.com>
2024-04-03 14:49:41 +0300

    appended new url routes

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('reported-incidents/', views.IncidentsAPIListView.as_view()),
+    path('incident/accidents', views.RoadAccidentsDetailView.as_view()),
+    path('incident/crimes', views.ReportedCrimesDetailView.as_view())
 ]
