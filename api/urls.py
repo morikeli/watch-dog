3eb14604e3bbe7a12763a548145774a766c69647
brocode <kelimbth@gmail.com>
2024-04-04 11:56:34 +0300

    appended new url paths

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('reported-incidents/', views.IncidentsAPIListView.as_view()),
     path('incident/accidents', views.RoadAccidentsDetailView.as_view()),
-    path('incident/crimes', views.ReportedCrimesDetailView.as_view())
+    path('incident/crimes', views.ReportedCrimesDetailView.as_view()),
+    path('wanted-suspects', views.WantedSuspectsDetailView.as_view()),
 ]
