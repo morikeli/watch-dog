c3cd7179f21e4897773b2d02e9e52dffb6884aa7
brocode <kelimbth@gmail.com>
2024-04-05 12:29:11 +0300

    added auth url patterns

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 
 urlpatterns = [
+    path('auth/login', views.LoginView.as_view()),
+    path('auth/signup', views.SignupView.as_view()),
     path('reported-incidents/', views.IncidentsAPIListView.as_view()),
     path('incident/accidents', views.RoadAccidentsDetailView.as_view()),
     path('incident/crimes', views.ReportedCrimesDetailView.as_view()),
