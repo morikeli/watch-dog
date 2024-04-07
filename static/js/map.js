8904333e13eb581ca46474fd50794122bbe06845
brocode <kelimbth@gmail.com>
2024-04-07 18:49:12 +0300

    made many changes 1.) implemented Haversine formula to calculate distance between accident spots and crimes scenes, and user's location 2.) moved forEach() inside function to get user current location

--- static/js/map.js
+++ static/js/map.js
@@ -74,47 +74,105 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map)
 
 
+// Use Haversine formula to calculate distance between two points. 
+// It is used to calculate the great-circle distance between two points on
+// the Earth's surface given their latitude and longitude coordinates. 
+function calculateDistance(lat1, lon1, lat2, lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = (lat2 - lat1) * Math.PI / 180;  // deg2rad below
+    var dLon = (lon2 - lon1) * Math.PI / 180;
+    var a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2)
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c; // Distance in km
+    console.log(d)
+    return d;
+}
+
 // Create FastMarkerCluster group for accident spots and crime scenes
 var accidentsClusterGroup = L.markerClusterGroup()
 var crimesClusterGroup = L.markerClusterGroup()
 
-// Add markers for accidents
-accident_spots.forEach(spot => {
-    latitude_coord = spot.latitude < 0 ? '&deg;S' : '&deg;N'
-    var marker = L.marker([spot.latitude, spot.longitude])
-    var popupContent = (
-        "<h5>Road accident</h5>" +
-        '<table class="table table-sm table-condensed table-striped table-bordered">' + "<tbody>" +
-        "<tr><td><b>Latitude:</b></td><td>" + spot.latitude + "<b>" + latitude_coord +  "</b>" + "</td>" +
-        "<tr><td><b>Longitude:</b></td><td>" + spot.longitude + "<b>&deg;E</b>" + "</td>" +
-        "<tr><td><b>County:</b></td><td>" + spot.county + "</td>" +
-        "<tr><td><b>Subcounty:</b></td><td>" + spot.sub_county + "</td>" +
-        "<tr><td><b>Road/Highway:</b></td><td>" + spot.road + "</td>" +
-        "<tr><td><b>Vehicle(s) involved:</b></td><td>" + spot.vehicles_count + "</td>" +
-        "<tr><td><b>Victim(s):</b></td><td>" + spot.road_user + "</td>" +
-        "<tr><td><b>Injuries:</b></td><td>" + spot.injuries_count + "</td>" +
-        "<tr><td><b>Fatalities:</b></td><td>" + spot.fatalities + "</td>" +
-        "</tbody>" + "</table>"
-    )
-    marker.bindPopup(popupContent).bindTooltip("Click for more info.")
-    accidentsClusterGroup.addLayer(marker)
+
+// Get user's current position and calculate distance from the accident spot
+navigator.geolocation.getCurrentPosition(function(position) {
+    // Retrieve latitude and longitude from the position object
+    var userLatitude = position.coords.latitude;
+    var userLongitude = position.coords.longitude;
+
+    accident_spots.forEach(spot => {
+        var distance = calculateDistance(userLatitude, userLongitude, spot.latitude, spot.longitude)
+        var distanceKilometers = (distance / 1000).toFixed(2); // Convert meters to kilometers
+        var distanceMiles = (distance * 0.000621371).toFixed(2); // Convert meters to miles
+    
+        var tooltipContent = "Distance to your location:<br>" +
+            "Meters: " + distance.toFixed(2) + " m<br>" +
+            "Kilometers: " + distanceKilometers + " km<br>" +
+            "Miles: " + distanceMiles + " mi"
+    
+        latitude_coord = spot.latitude < 0 ? '&deg;S' : '&deg;N'
+        var marker = L.marker([spot.latitude, spot.longitude])
+        var popupContent = (
+            "<h5>Road accident</h5>" +
+            '<table class="table table-sm table-condensed table-striped table-bordered">' + "<tbody>" +
+            "<tr><td><b>Latitude:</b></td><td>" + spot.latitude + "<b>" + latitude_coord +  "</b>" + "</td>" +
+            "<tr><td><b>Longitude:</b></td><td>" + spot.longitude + "<b>&deg;E</b>" + "</td>" +
+            "<tr><td><b>County:</b></td><td>" + spot.county + "</td>" +
+            "<tr><td><b>Subcounty:</b></td><td>" + spot.sub_county + "</td>" +
+            "<tr><td><b>Road/Highway:</b></td><td>" + spot.road + "</td>" +
+            "<tr><td><b>Vehicle(s) involved:</b></td><td>" + spot.vehicles_count + "</td>" +
+            "<tr><td><b>Victim(s):</b></td><td>" + spot.road_user + "</td>" +
+            "<tr><td><b>Injuries:</b></td><td>" + spot.injuries_count + "</td>" +
+            "<tr><td><b>Fatalities:</b></td><td>" + spot.fatalities + "</td>" +
+            "</tbody>" + "</table>"
+        )
+        marker.bindPopup(popupContent)
+        marker.bindTooltip(tooltipContent).openTooltip()
+        accidentsClusterGroup.addLayer(marker)
+    })
+
+}, function(error) {
+    alert("Error: ", error);
 })
 
-// Add markers for crimes
-crime_scenes.forEach(scene => {
-    latitude_coord = 'S' ? scene.latitude < 0 : 'N'
-    var marker = L.marker([scene.latitude, scene.longitude])
-    var popupContent = (
-        "<h5>Crime</h5>" +
-        '<table class="table table-sm table-condensed table-striped table-bordered">' + "<tbody>" +
-        "<tr><td><b>Latitude:</b></td><td>" + scene.latitude +  "<b>" + latitude_coord +  "</b>" + "</td>" +
-        "<tr><td><b>Longitude:</b></td><td>" + scene.longitude + "</td>" +
-        "<tr><td><b>County:</b></td><td>" + scene.county + "</td>" +
-        "<tr><td><b>Subcounty:</b></td><td>" + scene.sub_county + "</td>" +
-        "</tbody>" + "</table>"
-    )
-    marker.bindPopup(popupContent)
-    crimesClusterGroup.addLayer(marker)
+
+// Get user's current position and calculate distance from the accident spot
+navigator.geolocation.getCurrentPosition(function(position) {
+    // Retrieve latitude and longitude from the position object
+    var userLatitude = position.coords.latitude;
+    var userLongitude = position.coords.longitude;
+
+    // Add markers for crimes
+    crime_scenes.forEach(scene => {
+        var distance = calculateDistance(userLatitude, userLongitude, scene.latitude, scene.longitude)
+        var distanceKilometers = (distance / 1000).toFixed(2); // Convert meters to kilometers
+        var distanceMiles = (distance * 0.000621371).toFixed(2); // Convert meters to miles
+    
+        var tooltipContent = "Distance to your location:<br>" +
+            "Meters: " + distance.toFixed(2) + " m<br>" +
+            "Kilometers: " + distanceKilometers + " km<br>" +
+            "Miles: " + distanceMiles + " mi"
+    
+        latitude_coord = 'S' ? scene.latitude < 0 : 'N'
+        var marker = L.marker([scene.latitude, scene.longitude])
+        var popupContent = (
+            "<h5>Crime</h5>" +
+            '<table class="table table-sm table-condensed table-striped table-bordered">' + "<tbody>" +
+            "<tr><td><b>Latitude:</b></td><td>" + scene.latitude +  "<b>" + latitude_coord +  "</b>" + "</td>" +
+            "<tr><td><b>Longitude:</b></td><td>" + scene.longitude + "</td>" +
+            "<tr><td><b>County:</b></td><td>" + scene.county + "</td>" +
+            "<tr><td><b>Subcounty:</b></td><td>" + scene.sub_county + "</td>" +
+            "</tbody>" + "</table>"
+        )
+        marker.bindPopup(popupContent)
+        marker.bindTooltip(tooltipContent, {permanent: true}).openTooltip()
+        crimesClusterGroup.addLayer(marker)
+    })
+
+}, function(error) {
+    alert("Error: ", error);
 })
 
 // Add cluster groups to the map
