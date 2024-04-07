3eca0438884492e66391bc77dd767696ddc97072
brocode <kelimbth@gmail.com>
2024-04-07 18:50:25 +0300

    minor change - deleted unnecessary semicolon

--- static/js/map.js
+++ static/js/map.js
@@ -19,7 +19,7 @@ function onLocationFound(loc) {
     )
 
     // Set maxBounds for the map
-    map.setMaxBounds(maxBounds);    // limit max map view to user's current location
+    map.setMaxBounds(maxBounds)    // limit max map view to user's current location
     map.setView(loc.latlng, 14)     // zoom the map to user's current location. Zoom start - 14
     L.marker(loc.latlng, {icon: userIcon}).addTo(map).bindPopup("You are here!").openPopup()
 }
