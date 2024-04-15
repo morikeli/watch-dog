7f9980626f6b43b49b471be2edc033c1e7b7e1a8
brocode <kelimbth@gmail.com>
2024-04-15 17:10:17 +0300

    appended new url path

--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('auth/login', views.LoginView.as_view()),
     path('auth/signup', views.SignupView.as_view()),
     path('reported-incidents/', views.IncidentsAPIListView.as_view()),
+    path('incidents/location', views.IncidentLocationsListView.as_view()),
     path('incident/accidents', views.RoadAccidentsDetailView.as_view()),
     path('incident/crimes', views.ReportedCrimesDetailView.as_view()),
     path('wanted-suspects', views.WantedSuspectsDetailView.as_view()),
