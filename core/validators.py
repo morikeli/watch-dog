3c38ad71dbfb9a27397a7fc992e1abfceba08468
brocode <kelimbth@gmail.com>
2024-04-15 11:34:32 +0300

    created a validation function for incident_date

--- core/validators.py
+++ core/validators.py
@@ -1,6 +1,22 @@
 from django.http import HttpResponse
 from django.utils import timezone
+from django.core.exceptions import ValidationError
+from django.contrib import messages
 
+
+def validate_incident_date(value):
+    """
+    This function validates date provided in the input field `incident_date` in `ReportIncidentForm`.
+    Users cannot reported incidents that is past one week, i.e. occured more than a week ago (more than 7 days).
+    """
+    current_dt = timezone.now().date()
+    days_elapsed = current_dt - value
+
+    if value > current_dt:
+        raise ValidationError(f'Invalid date provided! Current date is "{current_dt}"')
+    
+    if days_elapsed.days > 7:
+        raise ValidationError("You can't report an incident that is past 1 week! Report the incident to the nearest police station.")
 
 def validate_last_seen_date(request):
     """ 
