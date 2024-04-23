116e84b7fe07ff67c94d0a0305289012e46295f3
brocode <kelimbth@gmail.com>
2024-04-23 17:46:03 +0300

    deleted unused imports

--- core/management/commands/load_data.py
+++ core/management/commands/load_data.py
@@ -1,6 +1,4 @@
-from django.db.models.signals import post_save, pre_save
 from django.core.management.base import BaseCommand
-from django.dispatch import receiver
 from django.conf import settings
 from core.models import Incident, IncidentLocation, RoadAccident
 import pandas as pd
